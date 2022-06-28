277a16602e0d82f2a4402a889c4627a8ac502241
keke <keke@yunify.com>
2022-06-28 15:54:22 +0800

    up: 添加企业云文档首页

--- static/js/config.js
+++ static/js/config.js
@@ -1,11 +1,5 @@
 const HIDEARR = []; // 需要隐藏的目录名
 
-const LOCATIONURL = "/compute/vm/intro/instance/";
-
-if (window) {
-  window.LOCATIONURL = LOCATIONURL;
-}
-
 if (window?.sessionStorage) {
   window?.sessionStorage.setItem("hideArr", HIDEARR);
 } else {
